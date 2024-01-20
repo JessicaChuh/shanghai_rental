a9308cfb34ed57c5aae1f04581335acb3815331e
Jessica Chuh <jessica_chuh@yahoo.com>
2024-01-20 17:25:26 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import pickle
+
+with open('random_forest_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+
+frame = pd.read_csv('frame.csv')
+
+def main():
+    # User inputs
+    # Number inputs
+    size = st.number_input("Size",min_value=0, max_value=600, step=1)
+    n_bedrooms = st.number_input("Number of Bedrooms",min_value=0, max_value=10, step=1)
+    n_bathrooms = st.number_input("Number of Bathrooms",min_value=0, max_value=10, step=1)
+    floor = st.number_input("Floor",min_value=1, max_value=50, step=1)
+
+    # lower text inputs
+    district = st.text_input("District").lower()
+    metro = st.text_input("Metro").lower()
+
+    # Checkbox inputs
+    furnished = st.checkbox("Furnished")
+    balcony = st.checkbox("Balcony")
+    oven = st.checkbox("Oven")
+    recently_renovated = st.checkbox("Recently Renovated")
+    air_filter = st.checkbox("Air Filter")
+    fitness_centers = st.checkbox("Fitness Centers")
+    floor_heating = st.checkbox("Floor Heating")
+    garden = st.checkbox("Garden")
+    historic_building = st.checkbox("Historic Building")
+    large_storage_room = st.checkbox("Large Storage Room")
+    parking = st.checkbox("Parking")
+    playground = st.checkbox("Playground")
+    pool = st.checkbox("Pool")
+    tennis_courts = st.checkbox("Tennis Courts")
+    wall_heating = st.checkbox("Wall Heating")
+    water_filter = st.checkbox("Water Filter")
+    pets_allowed = st.checkbox("Pets Allowed")
+
+
+    # Submit button
+    if st.button("Submit"):
+        # Create a dictionary to store the user input
+        user_input = {
+            'Size': size,
+            'N_Bedrooms': n_bedrooms,
+            'N_Bathrooms': n_bathrooms,
+            'Floor': floor,
+            'Furnished': int(furnished),
+            'District': district,
+            'Metro': metro,
+            'Balcony': int(balcony),
+            'Oven': int(oven),
+            'Recently renovated': int(recently_renovated),
+            'Air Filter': int(air_filter),
+            'Fitness Centers': int(fitness_centers),
+            'Floor Heating': int(floor_heating),
+            'Garden': int(garden),
+            'Historic Building': int(historic_building),
+            'Large Storage Room': int(large_storage_room),
+            'Parking': int(parking),
+            'Playground': int(playground),
+            'Pool': int(pool),
+            'Tennis Courts': int(tennis_courts),
+            'Wall heating': int(wall_heating),
+            'Water Filter': int(water_filter),
+            'Pets_allowed': int(pets_allowed)
+        }
+
+
+        # Create a DataFrame with the processed input
+        input_df = pd.DataFrame(user_input, index=[0])
+        frame[['Size', 'N_Bedrooms', 'N_Bathrooms', 'Floor', 'Furnished',
+                'Balcony', 'Oven', 'Recently renovated', 'Air Filter',
+                'Fitness Centers']] = input_df[['Size', 'N_Bedrooms', 'N_Bathrooms', 'Floor', 'Furnished', 'Balcony',
+            'Oven', 'Recently renovated', 'Air Filter', 'Fitness Centers']]
+
+        district = "District_" + input_df['District']
+        metro = "Metro_" + input_df['Metro']
+        frame[district] = 1
+        frame[metro] = 1
+
+
+        # Make the prediction
+        prediction = model.predict(frame)[0]
+
+
+        # Display the prediction
+        st.write("Predicted House Price:")
+        st.write(round(prediction))
+
+if __name__ == '__main__':
+    main()
